375dc6615e89338ad113c3b56f139d54458b42ec
Zola <zolamcadie@gmail.com>
2017-07-20 23:57:01 +0000

    added register post path

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,16 @@
 const express = require('express');
 const router  = express.Router();
 
+const generateRandomString = () => {
+  let output = '';
+  const base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * 62);
+    output += base[index];
+  }
+  return output;
+};
+
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
@@ -12,11 +22,28 @@ module.exports = (db) => {
       });
   });
 
-  router.get("/:id", (req, res) =>{
+  router.get("/:id", (req, res) => {
     db.findInTable('users', 'id', req.params.id)
       .then((results) => {
         res.json(results);
       });
+  });
+
+  router.get("/login/:name", (req, res) => {
+    db.findInTable('users', 'name', req.params.name)
+      .then((results) => {
+        res.json(results);
+      });
+  });
+
+  router.post("/register", (req, res) => {
+    const newUser = {
+      id: generateRandomString(),
+      name: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    };
+    db.addToTable('users', newUser);
   });
 
   return router;
