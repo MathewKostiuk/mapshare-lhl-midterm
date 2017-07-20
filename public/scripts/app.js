f793794f938fa1db9b05df0cb07648555b56eec4
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-20 21:18:30 +0000

    implemented multi-marker function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,32 +10,32 @@ $( function () {
   });
 
   initMap();
-
 });
 
- var json =
-  {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [125.6, 10.1]
-    },
-    "properties": {
-      "name": "Dinagat Islands"
-    }
-  }
 
-  function initMap() {
+  function initMap(json) {
         var victoria_bc = {lat: 48.428, lng: -123.365};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 12,
           center: victoria_bc
         });
-
-        var coords = json.geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1], coords[0]);
-        var marker = new google.maps.Marker({
-          position: latLng,
-          map: map
-        });
+        setMarkers(map);
       };
+
+var beaches = [
+  ['Bondi Beach', -33.890542, 151.274856, 4],
+  ['Coogee Beach', -33.923036, 151.259052, 5],
+  ['Cronulla Beach', -34.028249, 151.157507, 3],
+  ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+  ['Maroubra Beach', -33.950198, 151.259302, 1]
+];
+
+function setMarkers(map) {
+  for (var i = 0; i < beaches.length; i++) {
+    var beach = beaches[i];
+    var newMarker = new google.maps.Marker({
+      position: {lat: beach[1], lng: beach[2]},
+      map: map
+    });
+  }
+}
