8acc5b66ce1003867dedc6ee811702b11b81fdb7
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-20 19:45:48 +0000

    implemented marker on map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,4 +8,34 @@ $( function () {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });
+
+  initMap();
+
 });
+
+ var json =
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [125.6, 10.1]
+    },
+    "properties": {
+      "name": "Dinagat Islands"
+    }
+  }
+
+  function initMap() {
+        var victoria_bc = {lat: 48.428, lng: -123.365};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: victoria_bc
+        });
+
+        var coords = json.geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1], coords[0]);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
+        });
+      };
--- public/scripts/load-map.js
+++ public/scripts/load-map.js
@@ -1,12 +1,29 @@
-function initMap() {
-        var uluru = {lat: 48.428, lng: -123.365};
-        var test = {lat: 49.000, lng: -121.000};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: uluru
-        });
-        // var marker = new google.maps.Marker({
-          // position: uluru,
-          // map: map
-        // });
-      }
+// var map;
+
+//  var json =
+//   {
+//     "type": "Feature",
+//     "geometry": {
+//       "type": "Point",
+//       "coordinates": [125.6, 10.1]
+//     },
+//     "properties": {
+//       "name": "Dinagat Islands"
+//     }
+//   }
+
+
+// function initMap() {
+//         var victoria-bc = {lat: 48.428, lng: -123.365};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 12,
+//           center: victoria-bc
+//         });
+
+//         // var coords = json.geometry.coordinates;
+//         // var latLng = new google.maps.LatLng(coords[1], coords[0]);
+//         // var marker = new google.maps.Marker({
+//         //   position: latLng,
+//         //   map: map
+//         // });
+//       };
