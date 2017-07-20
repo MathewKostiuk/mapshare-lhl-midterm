fb930a0c033df5ae5ebf7434986c48b993b47a30
Zola <zolamcadie@gmail.com>
2017-07-20 22:22:34 +0000

    add database helper to insert data to table

--- db/index.js
+++ db/index.js
@@ -4,6 +4,12 @@ const ENV         = process.env.ENV || "development";
 const knexConfig  = require("../knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 
+//////////////////////////////////////////////////////////////////
+//////                                                         ///
+///// utility functions to communicate with database,         ////
+////  exported to server.js, intended to be passed to routes /////
+///                                                         //////
+//////////////////////////////////////////////////////////////////
 const findInDatabase = (table, column, match) => {
   console.log('searching');
   return knex.select().from(table)
@@ -17,4 +23,21 @@ const findInDatabase = (table, column, match) => {
     });
 };
 
-module.exports = {knex, findInDatabase};
+// takes an object of key value pairs in the format of:
+// {keyname: value, otherkeyname: othervalue}
+// these pairs will be added to specified table
+const addToDatabase = (table, keyValuePairs) => {
+  console.log('updating');
+  knex.insert(keyValuePairs)
+    .into(table)
+    .catch((err) => {
+      if (err) {
+        return console.error(err);
+      }
+    })
+    .then(() => {
+      console.log('updated');
+    })
+}
+
+module.exports = {knex, findInDatabase, addToDatabase};
