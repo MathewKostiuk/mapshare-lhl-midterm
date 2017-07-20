b574b0aea74016c61c13ad15e2525e5d77d3a611
Zola <zolamcadie@gmail.com>
2017-07-20 15:49:32 +0000

    seeded database with test data

--- db/migrations/20170719234922_create_user_favourites_table.js
+++ db/migrations/20170719234922_create_user_favourites_table.js
@@ -1,11 +1,7 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('user_favourites', (table) => {
-    table.integer('user_id')
-    .references('id')
-    .inTable('users');
-    table.integer('list_id')
-    .references('id')
-    .inTable('lists');
+    table.integer('user_id');
+    table.integer('list_id');
   });
 };
 
