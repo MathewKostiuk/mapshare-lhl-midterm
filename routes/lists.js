a1825814ac863a22f17963972b91f16e2e6820cb
Zola <zolamcadie@gmail.com>
2017-07-21 20:11:10 +0000

    added route to view all lists

--- routes/lists.js
+++ routes/lists.js
@@ -5,6 +5,13 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
+  router.get("/", (req, res) => {
+    db.viewTable("lists")
+      .then((results) => {
+        res.json(results);
+      });
+  });
+
   router.get("/:id", (req, res) => {
     db.findInTable("items", "list_id", req.params.id)
       .then((results) => {
