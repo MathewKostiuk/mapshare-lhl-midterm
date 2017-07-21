096bf720f9f616b3ac325edff1a5124ff08c8c65
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-21 16:44:06 +0000

    edits

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $form.on("submit", function(event) {
 });
 
 var infos = [];
-var formStr = "<form action='/items/new/' method='POST' id='newItem'><input name type='text' id='markerName' placeholder='Name:'/><br><input name=text type='text' id='markerDescription' placeholder='Description:'/><br><input name='img_url' type='text' id='markerImage' placeholder='Image URL:'/><br><input type='button' value='submit'/></form>"
+// var formStr = "<form action='/items/new/' method='POST' id='newItem'><input name type='text' id='markerName' placeholder='Name:'/><br><input name=text type='text' id='markerDescription' placeholder='Description:'/><br><input name='img_url' type='text' id='markerImage' placeholder='Image URL:'/><br><input type='button' value='submit'/></form>"
 var textBox = [];
 
 var points = [
