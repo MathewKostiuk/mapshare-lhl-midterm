83f91272ef87c72cacf22ed0fca2fd74687936ab
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-22 00:04:24 +0000

    fixed 'not an instance of map' error

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,7 @@ const $form = $("#register-form");
 $form.on("submit", function(event) {
   event.preventDefault();
 });
-
+var map;
 
 var infos = [];
 var formStr = "<form action='/items/new/' method='POST' id='newItem'><input name type='text' id='markerName' placeholder='Name:'/><br><input name=text type='text' id='markerDescription' placeholder='Description:'/><br><input name='img_url' type='text' id='markerImage' placeholder='Image URL:'/><br><input type='submit' value='submit'/></form>"
