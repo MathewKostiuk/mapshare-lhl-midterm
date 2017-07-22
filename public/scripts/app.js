3c0d6009ad7819cc166cc1b18035751b50e765d3
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-22 20:09:14 +0000

    auto-focus on new markers when list is populated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,7 @@ $form.on("submit", function(event) {
 var map;
 
 var infos = [];
-var formStr = "<form action='/items/new/' method='POST' id='newItem'><input name type='text' id='markerName' placeholder='Name:'/><br><input name=text type='text' id='markerDescription' placeholder='Description:'/><br><input name='img_url' type='text' id='markerImage' placeholder='Image URL:'/><br><input type='submit' value='submit'/></form>"
+var formStr = "<form action='/items/new/' method='POST' id='newItem'><input name type='text' id='markerName' placeholder='Name:'/><br><input name=text type='text' id='markerDescription' placeholder='Description:'/><br><input name='img_url' type='text' id='markerImage' placeholder='Image URL:'/><br><input type='submit' value='submit'/></form>";
 var textBox = [];
 
 
@@ -59,21 +59,27 @@ function closeInfos() {
 
 function closeTextBox(){
 
-   if(textBox.length > 0){
+  if(textBox.length > 0){
 
-      /* detach the info-window from the marker ... undocumented in the API docs */
-      textBox[0].set("marker", null);
+    /* detach the info-window from the marker ... undocumented in the API docs */
+    textBox[0].set("marker", null);
 
-      /* and close it */
-      textBox[0].close();
+    /* and close it */
+    textBox[0].close();
 
-      /* blank the array */
-      textBox.length = 0;
-   }
+    /* blank the array */
+    textBox.length = 0;
+  }
 }
 
 
 function setMarkers(map, items) {
+  if (!items) {
+    return;
+  }
+
+  var infowindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
   for (var i = 0; i < items.length; i++) {
     var item = items[i];
     var content = item.name;
@@ -82,9 +88,10 @@ function setMarkers(map, items) {
       position: {lat: item.latitude, lng: item.longitude},
       map: map
     });
-    var contentString = `<p>${item.name}</p><p>${item.description}</p><img src='${item.image_url}'>`
+    bounds.extend(marker.position);
+
+    var contentString = `<p>${item.name}</p><p>${item.description}</p><img src='${item.image_url}'>`;
     marker.setMap(map);
-    var infowindow = new google.maps.InfoWindow();
     infowindow.setContent(contentString);
     google.maps.event.addListener(marker, 'click', (function(marker, infowindow){
       return function() {
@@ -94,24 +101,25 @@ function setMarkers(map, items) {
       };
     }(marker, infowindow)));
   }
+  map.fitBounds(bounds);
 }
 
 
 
-function initMap() {
+function initMap(items) {
   var victoriaBc = {lat: 48.428, lng: -123.365};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: victoriaBc
   });
-   // setMarkers(map, items);
+  setMarkers(map, items);
 
   google.maps.event.addListener(map, "click", function (event) {
     closeTextBox();
     var infowindow = new google.maps.InfoWindow();
     var latitude = event.latLng.lat();
     var longitude = event.latLng.lng();
-    var formStr = `<form action='/items/new' method='POST' id='new-item'><input name='name' type='name' id='markerName' placeholder='Name:'><br><input name='description' type='text' id='markerDescription' placeholder='Description:'><br><input name='img' type='url' id='markerImage' placeholder='http://imgurl.com'><br><input type='text' name='lat' value=${latitude} readonly><input type='text' name='long' value=${longitude} readonly><input type='submit' value='new-item'/></form>`
+    var formStr = `<form action='/items/new' method='POST' id='new-item'><input name='name' type='name' id='markerName' placeholder='Name:'><br><input name='description' type='text' id='markerDescription' placeholder='Description:'><br><input name='img' type='url' id='markerImage' placeholder='http://imgurl.com'><br><input type='text' name='lat' value=${latitude} readonly><input type='text' name='long' value=${longitude} readonly><input type='submit' value='new-item'/></form>`;
 
     infowindow.setContent(formStr);
     infowindow.setPosition(event.latLng);
@@ -119,6 +127,9 @@ function initMap() {
     textBox[0] = infowindow;
     var $newItem = $('#new-item');
     $newItem.on("submit", handleNewItem);
+    return new Promise(function(resolve, reject) {
+      resolve(map);
+    });
   });
 }
 
@@ -131,14 +142,13 @@ function handleNewItem(event) {
     data: form
   })
     .done(closeTextBox());
-};
+}
 
 
 
 // Document Ready
 $( function () {
-var listsUrl = "/lists/";
-var items = '';
+  var listsUrl = "/lists/";
   utils.request("GET", "/lists").then(function (lists) {
     for(list of lists) {
       $("<a>").text(list.name).attr('class', 'list-item').attr('id', list.id).append("<br>").appendTo($("#left-col"));
@@ -147,13 +157,8 @@ var items = '';
         event.stopImmediatePropagation();
         var id = listsUrl + $(this).attr('id');
         utils.request("GET", id).then(function (items) {
-        var victoriaBc = {lat: 48.428, lng: -123.365};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: victoriaBc
+          initMap(items);
         });
-         setMarkers(map, items);
-        })
       });
     }
   });
